a005a9b0927eaf2c09233c212b4ffd12c9488202
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-08-21 22:26:48 -0400

    finish displaying random movie

--- assets/script.js
+++ assets/script.js
@@ -34,7 +34,7 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch('https://imdb-api.com/en/API/Top250Movies/k_9fv35bw7')
+fetch('https://imdb-api.com/en/API/Top250Movies/k_va4qdhl7')
 .then(function(response){
   return response.json();
 })
@@ -43,10 +43,34 @@ fetch('https://imdb-api.com/en/API/Top250Movies/k_9fv35bw7')
   
   var movies = document.querySelectorAll('.movie');
   console.log(movies.length);
+  var randomMovies = document.querySelectorAll('.randomMovie');
   for(var i = 0; i < movies.length; i++){
-    var randomIndex = Math.floor(Math.random() * 10);
-    console.log(i);
-    movies[i].src = data.items[i].image;
+
+    var fullTitle = document.createElement('a');
+    var ratingHeading = document.createElement('p');
+    var button  = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.setAttribute('class', 'w3-light-blue w3-text-white w3-hover-cyan');
+    button.textContent = 'Add to list';
+    var randomIndex = Math.floor(Math.random() * 249);
+  
+    console.log(data.items[randomIndex].fullTitle);
+    fullTitle.textContent = data.items[randomIndex].title;
+    fullTitle.style.display = 'block';
+    fullTitle.setAttribute('href', 'https://www.imdb.com/title/' + data.items[randomIndex].id);
+    ratingHeading.textContent = data.items[randomIndex].imDbRating;
+    var icon = document.createElement('i');
+    icon.setAttribute('class', 'fa fa-star');
+    ratingHeading.append(icon);
+    
+    movies[i].src = data.items[randomIndex].image;
+    randomMovies[i].append(fullTitle);
+    randomMovies[i].append(ratingHeading);
+    randomMovies[i].append(button);
+    randomMovies[i].style.display = 'table';
+    randomMovies[i].setAttribute('style', 'overflow: auto');
+  
+
   }
 });
  
