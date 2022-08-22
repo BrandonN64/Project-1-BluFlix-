f36f5e2632a0af573c3b886eb0998ce84db1bffe
BrandonN64 <Brandon.nastri@gmail.com>
2022-08-21 20:11:09 -0400

    Changed the google maps API so it no longer searches for the users location, and instead displays the location of movie theaters in Connecticut

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Project-1-Bluluflix-
-This is the first project that we were assigned
+
+# Description
+Description of the project: This is a website that lets you browse movies, see the ratings on IMDB, and search for movies that you want to see. It also allows you to see nearby movie theaters so you have the ability to find nearby movie theaters. This website was designed with a mobile-first approach that way anyone would be able to access it, even on the go. 
+
+# Technologies Used
+
+w3.css Framework
+
+Google Maps API
+
+IMDB API
+
+# Features we Added
+
+Added a search bar that calls to the IMDB API whenever you search, and shows the results of the search on the page
+
+Added randomized movies to the main page of the HTML
+
+Added a navbar that has multiple options
+
+Made the website with a mobile-first approach, so that it works on all screen sizes
+
+Desktop Image:
+
+Mobile Image:
+
+Added google maps api to show the nearby theaters in Connecticut
+
+Added a list that shows you movies you want to see
+
+
+# Features we wanted to add
+
+Movie Showtimes so you can see what movies are showing near you, and when they are showing
+Didnt get added because the api was made for Ruby, which was a different programming language.
+
+Wanted to add a way to reserve tickets through the website
+Didnt get added because we could not find any compatible API's to make reservations through movie theaters
+
+Wanted to add directions to the closest movie theater through google maps
+Didnt get added due to the limitations of the Javascript Maps API and Google Places API
+
+Wanted to make the name Bluluflix+
+Didnt add it because we thought it was too many syllables and seems like a rushed / poorly thought of name
+
+Wanted to make the logo clickable as a way to return to the homepage
+Didnt get added due to the use of display:none
+
+Wanted to display the google map on the bottom of the homepage
+Didn't get added due to the time it took for the map to load, making the page look unresponsive and poorly made.
+
+Wanted to add a footer to the website with a button that brought you to the top of the page
+Didnt get added because the page was a lot smaller then we originally planned it to be, due to the requirements of the MVP of the page
--- assets/script.js
+++ assets/script.js
@@ -29,193 +29,164 @@ var buttonClickHandler = function(event) {
 	var genre = event.target.getAttribute('genre');
   console.log(genre)
 }
-let map, infoWindow;
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 41.700540, lng: -72.559270 },
-    zoom: 9,
-  });
-  infoWindow = new google.maps.InfoWindow();
-  const locationButton = document.createElement("button");
+	var myMapCenter = { lat: 41.700540, lng: -72.559270 };
 
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
+	// Create a map object and specify the DOM element for display.
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: myMapCenter,
+		zoom: 9
+	});
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
+	function marktheater(theaterInfo){
+
+    const icon = {
+      url: "https://maps.gstatic.com/mapfiles/place_api/icons/v2/movie_pinlet.svg", // url
+      scaledSize: new google.maps.Size(25, 25), // scaled size
+      origin: new google.maps.Point(0,0), // origin
+      anchor: new google.maps.Point(0, 0)
+    };
+
+		// Create a marker and set its position.
+		var marker = new google.maps.Marker({
+			map: map,
+			position: theaterInfo.location,
+			title: theaterInfo.name,
+      icon: icon
+		});
+
+		// show theater info when marker is clicked
+		marker.addListener('click', function(){
+			showtheaterInfo(theaterInfo);
+		});
+	}
+
+	// show theater info in text box
+	function showtheaterInfo(theaterInfo){
+		var info_div = document.getElementById('info_div');
+		info_div.innerHTML = 'Theater Name: '
+			+ theaterInfo.name
+			+ '<br>Hours: ' + theaterInfo.hours;
+	}
+  var theaters = [
+    {
+      name: 'Art Cinema',
+      location: {lat:41.743630, lng:-72.676730},
+      hours:'12pm to 9pm'
+    },
+    {
+      name: 'Apple Cinemas Xtreme',
+      location: {lat:41.747246, lng:-72.712448},
+      hours:'11am to 10pm'
+    },
+    {
+      name: 'Apple Cinemas Luxury',
+      location: {lat:41.762770, lng:-72.671250},
+      hours:'12pm to 11:30pm'
+    },
+    {
+      name: 'Cinépolis Luxury Cinemas',
+      location: {lat:41.760200, lng:-72.742040},
+      hours:'3pm to 11pm'
+    },
+    {
+      name: 'Picture Show Berlin',
+      location: {lat:41.629181, lng:-72.742882},
+      hours:'10am to 11pm'
+    },
+    {
+      name: 'Parkade Cinemas and Entertainment',
+      location: {lat:41.780609, lng:-72.541832},
+      hours:''
+    },
+    {
+      name: 'Cinemark Buckland Hills 18 and IMAX',
+      location: {lat:41.801041, lng:-72.547752},
+      hours:''
+    },
+    {
+      name: 'AMC Plainville 20',
+      location: {lat:41.676571, lng:-72.841614},
+      hours:''
+    },
+    {
+      name: 'Metro Movies 12',
+      location: {lat:41.558281, lng:-72.648193},
+      hours:''
+    },
+    {
+      name: 'FunFlicks of Connecticut',
+      location: {lat:41.715570, lng:-72.667510},
+      hours:''
+    },
+    {
+      name: 'Apple Cinemas Simsbury 8',
+      location: {lat:41.817379, lng:-72.865067},
+      hours:''
+    },
+    {
+      name: 'Lincoln Theater',
+      location: {lat:41.793789, lng:-72.716568},
+      hours:''
+    },
+    {
+      name: 'Holiday Cinemas Stadium 14',
+      location: {lat:41.487640, lng:-72.808060},
+      hours:''
+    },
+    {
+      name: 'AMC Southington 12',
+      location: {lat:41.565540, lng:-72.908070},
+      hours:''
+    },
+    {
+      name: 'Apple Cinemas Waterbury',
+      location: {lat:41.564892, lng:-73.007080},
+      hours:''
+    },
+    {
+      name: 'Mansfield Movieplex 8',
+      location: {lat:41.754410, lng:-72.193120},
+      hours:''
+    },
+    {
+      name: 'Agawam Cinemas',
+      location: {lat:42.060070, lng:-72.632460},
+      hours:''
+    },
+    {
+      name: 'Apple Cinemas Barkhamsted 9',
+      location: {lat:41.817379, lng:-72.865067},
+      hours:''
+    },
+    {
+      name: 'Cinemark West Springfield 15',
+      location: {lat:42.128580, lng:-72.630060},
+      hours:''
+    },
+    {
+      name: 'Cinemark North Haven',
+      location: {lat:41.359180, lng:-72.873848},
+      hours:''
+    },
+    {
+      name: 'Cine 4',
+      location: {lat:41.327461, lng:-72.879051},
+      hours:''
+    },
+    {
+      name: 'Marquee Cinemas',
+      location: {lat:41.327461, lng:-72.879051},
+      hours:''
+    },
+    {
+      name: 'Gilson Cafe & Cinema',
+      location: {lat:41.921330, lng:-73.069510},
+      hours:''
     }
-  });
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
-}
-var stores = [
-  {
-    name: 'Art Cinema',
-    location: {lat:41.743630, lng:-72.676730},
-    hours:'12pm to 9pm'
-  },
-  {
-    name: 'Apple Cinemas Xtreme',
-    location: {lat:41.747246, lng:-72.712448},
-    hours:'11am to 10pm'
-  },
-  {
-    name: 'Apple Cinemas Luxury',
-    location: {lat:41.762770, lng:-72.671250},
-    hours:'12pm to 11:30pm'
-  },
-  {
-    name: 'Cinépolis Luxury Cinemas',
-    location: {lat:41.760200, lng:-72.742040},
-    hours:'3pm to 11pm'
-  },
-  {
-    name: 'Picture Show Berlin',
-    location: {lat:41.629181, lng:-72.742882},
-    hours:'10am to 11pm'
-  },
-  {
-    name: 'Parkade Cinemas and Entertainment',
-    location: {lat:41.780609, lng:-72.541832},
-    hours:''
-  },
-  {
-    name: 'Cinemark Buckland Hills 18 and IMAX',
-    location: {lat:41.801041, lng:-72.547752},
-    hours:''
-  },
-  {
-    name: 'AMC Plainville 20',
-    location: {lat:41.676571, lng:-72.841614},
-    hours:''
-  },
-  {
-    name: 'Metro Movies 12',
-    location: {lat:41.558281, lng:-72.648193},
-    hours:''
-  },
-  {
-    name: 'FunFlicks of Connecticut',
-    location: {lat:41.715570, lng:-72.667510},
-    hours:''
-  },
-  {
-    name: 'Apple Cinemas Simsbury 8',
-    location: {lat:41.817379, lng:-72.865067},
-    hours:''
-  },
-  {
-    name: 'Lincoln Theater',
-    location: {lat:41.793789, lng:-72.716568},
-    hours:''
-  },
-  {
-    name: 'Holiday Cinemas Stadium 14',
-    location: {lat:41.487640, lng:-72.808060},
-    hours:''
-  },
-  {
-    name: 'AMC Southington 12',
-    location: {lat:41.565540, lng:-72.908070},
-    hours:''
-  },
-  {
-    name: 'Apple Cinemas Waterbury',
-    location: {lat:41.564892, lng:-73.007080},
-    hours:''
-  },
-  {
-    name: 'Mansfield Movieplex 8',
-    location: {lat:41.754410, lng:-72.193120},
-    hours:''
-  },
-  {
-    name: 'Agawam Cinemas',
-    location: {lat:42.060070, lng:-72.632460},
-    hours:''
-  },
-  {
-    name: 'Apple Cinemas Barkhamsted 9',
-    location: {lat:41.817379, lng:-72.865067},
-    hours:''
-  },
-  {
-    name: 'Cinemark West Springfield 15',
-    location: {lat:42.128580, lng:-72.630060},
-    hours:''
-  },
-  {
-    name: 'Cinemark North Haven',
-    location: {lat:41.359180, lng:-72.873848},
-    hours:''
-  },
-  {
-    name: 'Cine 4',
-    location: {lat:41.327461, lng:-72.879051},
-    hours:''
-  },
-  {
-    name: 'Marquee Cinemas',
-    location: {lat:41.327461, lng:-72.879051},
-    hours:''
-  },
-  {
-    name: 'Gilson Cafe & Cinema',
-    location: {lat:41.921330, lng:-73.069510},
-    hours:''
-  }
-]
-function markStore(storeInfo){
-
-	// Create a marker and set its position.
-	var marker = new google.maps.Marker({
-		map: map,
-		position: storeInfo.location,
-		title: storeInfo.name
+  ]
+	theaters.forEach(function(theater){
+		marktheater(theater);
 	});
 
-	// show store info when marker is clicked
-	marker.addListener('click', function(){
-		showStoreInfo(storeInfo);
-	});
 }
-
-// show store info in text box
-function showStoreInfo(storeInfo){
-	var info_div = document.getElementById('info_div');
-	info_div.innerHTML = 'Store name: '
-		+ storeInfo.name
-		+ '<br>Hours: ' + storeInfo.hours;
-}
-stores.forEach(function(store){
-	markStore(store);
-});
